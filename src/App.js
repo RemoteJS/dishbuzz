5480f3891fa20b23d6b17196497b826820afae19
Christian Maniewski <chmanie@fastmail.com>
2017-09-13 17:25:07 +0200

    Move to styled-components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 // @flow
 
 import React, { Component } from 'react'
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg'
-import './App.css'
 
-type Props = {}
+type Props = {
+  foo: number,
+  bar?: string,
+}
+
+const spin = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
+`
+
+const Main = styled.main`text-align: center;`
+
+const MainHeader = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+
+const AppLogo = styled.img`
+  animation: ${spin} infinite 20s linear;
+  height: 80px;
+`
+
+const AppIntro = styled.p`font-size: large;`
 
 class App extends Component<Props> {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Main>
+        <MainHeader>
+          <AppLogo src={logo} alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        </MainHeader>
+        <AppIntro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </AppIntro>
+      </Main>
     )
   }
 }
